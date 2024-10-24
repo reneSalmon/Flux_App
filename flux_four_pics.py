850bfb7ca97479293c7355f1d772dcd8b8ded343
René Salmon <rene.salmon@outlook.de>
2024-10-24 13:38:44 +0200

    API key not found issue - pkg resource

--- flux_four_pics.py
+++ flux_four_pics.py
@@ -4,6 +4,7 @@ import time
 from io import BytesIO
 from PIL import Image
 import os
+from dotenv import load_dotenv
 import pkg_resources
 installed_packages = pkg_resources.working_set
 for package in installed_packages:
